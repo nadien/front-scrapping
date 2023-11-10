40c74955c9916258495e8db58304215abcd8192a
nadien <sergiomen4@gmail.com>
2023-11-09 20:07:52 -0600

    soriana done, title, price, desc, code

--- soriana/scraper.py
+++ soriana/scraper.py
@@ -13,21 +13,34 @@ response = requests.get(url, headers=headers)
 print(response)
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
-    product_titles = []
-    product_prices = []
+    product_title = []
+    product_price = []
+    product_description = []
+    product_code = []
 
     for product in soup.find_all("div", class_="product-detail__content-container"):
         price = product.find("span", class_="cart-price").text
-        product_prices.append(price)
+        product_price.append(price)
 
     for product in soup.find_all("div", class_="c-product-name"):
         title = product.find("h1", class_="product-name").text
-        product_titles.append(title)
+        product_title.append(title)
 
-    for title, price in zip(product_titles, product_prices):
+    for product in soup.find_all("div", class_="description-and-detail Soriana"):
+        desc = product.find("div", id="videosMobile").text
+        product_description.append(desc)
+    
+    #EAN code
+    for product in soup.find_all("script"):
+        code = product.get("data-flix-ean")
+        if code is not None:
+            product_code.append(code)
+
+    for title, price, desc, code in zip(product_title, product_price, product_description, product_code):
         print("Title:", title)
         print("Price:", price)
-        print()
+        print("Description:", desc)
+        print("EAN code:", code)
 
 else:
     print("Failed to retrieve the webpage. Status code:", response.status_code)
